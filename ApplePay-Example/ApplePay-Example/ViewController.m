1ecfe917db94b1fd93151ddff39256ce17ad709e
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-08-30 11:49:14 -0300

    Implementing methods of PKPaymentAuthorizationViewControllerDelegate

--- ApplePay-Example/ApplePay-Example/ViewController.m
+++ ApplePay-Example/ApplePay-Example/ViewController.m
@@ -10,7 +10,7 @@
 
 #import <PassKit/PassKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<PKPaymentAuthorizationViewControllerDelegate>
 
 @end
 
@@ -22,6 +22,97 @@
 
 -(void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - PKPaymentAuthorizationViewControllerDelegate methods
+
+-(void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
+                      didAuthorizePayment:(PKPayment *)payment
+                               completion:(void (^)(PKPaymentAuthorizationStatus status))completion {
+    
+    [self handlePaymentAuthorizationWithPayment:payment completion:completion];
+    
+}
+
+-(void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+#pragma mark - Private methods
+
+-(void)handlePaymentAuthorizationWithPayment:(PKPayment *)payment
+                                  completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+    
+    // Try to create a token with the payment
+    [self createTokenWithPayment:payment completion:^(PKPaymentToken *token, NSError *error) {
+       
+        if (error) {
+            // Failed
+            completion(PKPaymentAuthorizationStatusFailure);
+            return;
+        }
+        
+        [self createBackendChargeWithToken:token completion:completion];
+        
+    }];
+    
+}
+
+-(void)createTokenWithPayment:(PKPayment *)payment
+                   completion:(void(^)(PKPaymentToken *token, NSError *error))completion {
+    
+    // Try to create a token with the payment
+    PKPaymentToken *token = payment.token;
+    
+    NSError *error;
+    
+    if ( ! token ) {
+        NSDictionary *userInfo = @{NSLocalizedDescriptionKey:@"Error: Failed to create the token."};
+        error = [NSError errorWithDomain:@"MyDomain" code:0 userInfo:userInfo];
+    }
+    
+    if ( completion )
+        completion(token,error);
+
+}
+
+-(void)createBackendChargeWithToken:(PKPaymentToken *)token
+                         completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+    
+    NSURL *url = [NSURL URLWithString:@"https://example.com/token"];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    request.HTTPMethod = @"POST";
+    
+    NSMutableString *parametersInBody = [NSMutableString new];
+    
+    [parametersInBody appendFormat:@"paymentInstrumentName=%@",token.paymentInstrumentName];
+    [parametersInBody appendFormat:@"&paymentNetwork=%@",token.paymentNetwork];
+    [parametersInBody appendFormat:@"&transactionIdentifier=%@",token.transactionIdentifier];
+    [parametersInBody appendFormat:@"&paymentData=%@",[NSString stringWithUTF8String:[token.paymentData bytes]]];
+    
+    NSString *body     = [parametersInBody copy];
+    
+    request.HTTPBody   = [body dataUsingEncoding:NSUTF8StringEncoding];
+    
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response,
+                                               NSData *data,
+                                               NSError *error) {
+                               if (error) {
+                                   completion(PKPaymentAuthorizationStatusFailure);
+                               } else {
+                                   completion(PKPaymentAuthorizationStatusSuccess);
+                               }
+                               
+                           }];
+    
+    return;
+    
+
+    
 }
 
 @end
